6cd569f1698b1f7b020bc038bd5c9e39ed16daa9
tarasshcheban <shcherbantaras07@gmail.com>
2024-08-26 15:47:35 +0300

    Select best answers

---
+++ js-tasks-03/tasks-03.js
@@ -0,0 +1,62 @@
+//Find the smallest integer in the array
+//https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascripthttps://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+function findSmallestInt(arr) {
+  return Math.min(...arr);
+}
+
+//Geometry Basics: Circle Circumference in 2D
+//https://www.codewars.com/kata/58e43389acfd3e81d5000a88/javascript
+function circleCircumference(circle) {
+  return +(2 * Math.PI * circle.radius).toFixed(6);
+}
+
+//Training JS #12: loop statement --for..in and for..of
+//https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+function giveMeFive(obj) {
+  const array = [];
+  for (let key in obj) {
+    if (key.length === 5) array.push(key);
+    if (obj[key].length === 5) array.push(obj[key]);
+  }
+  return array;
+}
+
+//Understanding closures - the basics
+//https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+function buildFun(n){
+	const res = []
+	for (let i = 0; i< n; i++){
+		res.push(function(){
+			return(i)
+		})
+	}
+	return res
+}
+
+//Fun with ES6 Classes #2 - Animals and Inheritance
+//https://www.codewars.com/kata/56f935002e6c0d55fa000d92/solutions/javascript
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status);
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
