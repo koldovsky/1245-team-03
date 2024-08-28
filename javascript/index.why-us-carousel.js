2eae24f1ae862dbc6b29105c48322c2a29bf4713
Olexiy Shumylo <you@example.com>
2024-08-28 14:37:52 +0300

    updated why-us carousel

--- javascript/index.why-us-carousel.js
+++ javascript/index.why-us-carousel.js
@@ -5,41 +5,40 @@ const slides = [
   '<div class="why__us-item"><div class="why__us-image-container"><img src="https://res2.weblium.site/res/5c78f7951f572b0022f79c95/5c7939d626c3d000250bbf9c" alt="craftsmanship" class="why__us-image"></div><h3 class="why__us-item-title">Craftsmanship</h3><p>We combine traditional techniques and modern manufacturing technologies to create exquisite yet durable pieces.</p></div>',
 ];
 
-let currentSlideIdx = 0;
+let currentIdxOfSlides = 0;
 
-function renderSlide() {
-  const slideContainer = document.querySelector(".why__us-container");
-  slideContainer.innerHTML = slides[currentSlideIdx];
+function renderSlides() {
+  const whyUsContainer = document.querySelector(".why__us-container");
+  whyUsContainer.innerHTML = slides[currentIdxOfSlides];
   if (window.matchMedia("(min-width: 768px)").matches) {
-    const secondSlideIdx =
-      currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-    slideContainer.innerHTML += slides[secondSlideIdx];
+    const secondIdxOfSlides =
+      currentIdxOfSlides + 1 >= slides.length ? 0 : currentIdxOfSlides + 1;
+    whyUsContainer.innerHTML += slides[secondIdxOfSlides];
     if (window.matchMedia("(min-width: 1024px)").matches) {
-      const thirdSlideIdx =
-        secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
-      slideContainer.innerHTML += slides[thirdSlideIdx];
+      const thirdIdxOfSlides =
+        secondIdxOfSlides + 1 >= slides.length ? 0 : secondIdxOfSlides + 1;
+      whyUsContainer.innerHTML += slides[thirdIdxOfSlides];
     }
   }
 }
 
 function nextSlide() {
-  currentSlideIdx =
-    currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
-  renderSlide();
+  currentIdxOfSlides =
+    currentIdxOfSlides + 1 >= slides.length ? 0 : currentIdxOfSlides + 1;
+  renderSlides();
 }
 
 function prevSlide() {
-  currentSlideIdx =
-    currentSlideIdx - 1 < 0 ? slides.length - 1 : currentSlideIdx - 1;
-  renderSlide();
+  currentIdxOfSlides =
+    currentIdxOfSlides - 1 < 0 ? slides.length - 1 : currentIdxOfSlides - 1;
+  renderSlides();
 }
+renderSlides();
 
-renderSlide();
+const nextSlideClick = document.querySelector(".why-us-carousel__btn-next");
+nextSlideClick.addEventListener("click", nextSlide);
 
-const btnNext = document.querySelector(".why-us-carousel__btn-next");
-btnNext.addEventListener("click", nextSlide);
+const prevSlideClick = document.querySelector(".why-us-carousel__btn-prev");
+prevSlideClick.addEventListener("click", prevSlide);
 
-const btnPrev = document.querySelector(".why-us-carousel__btn-prev");
-btnPrev.addEventListener("click", prevSlide);
-
-window.addEventListener("resize", renderSlide);
+window.addEventListener("resize", renderSlides);
