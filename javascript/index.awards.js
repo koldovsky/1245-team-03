63aabd48b8dd1acc6e1479a6c4733560f716772a
tarasshcheban <shcherbantaras07@gmail.com>
2024-09-09 23:01:38 +0300

    Fix closing modal window

--- javascript/index.awards.js
+++ javascript/index.awards.js
@@ -1,3 +1,4 @@
+// Відкрити модальне вікно при натисканні на елемент
 document.querySelectorAll('.awards__item').forEach(item => {
     item.addEventListener('click', () => {
         const modal = document.querySelector('.modal');
@@ -9,4 +10,18 @@ document.querySelectorAll('.awards__item').forEach(item => {
         
         modal.style.display = 'flex';
     });
+});
+
+// Закрити модальне вікно при натисканні на хрестик
+document.querySelector('.modal-close').addEventListener('click', () => {
+    const modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+});
+
+// Додатково: Закрити модальне вікно при натисканні за межами вікна
+window.addEventListener('click', (event) => {
+    const modal = document.querySelector('.modal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 });
